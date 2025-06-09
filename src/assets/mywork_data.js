54f64882d66ecf027ec4a0f1cd3352c8dfc336ce
Krish Sahu <131345914+Krish3024@users.noreply.github.com>
2025-06-09 13:53:03 +0530

    Update mywork_data.js

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,7 +1,7 @@
 import project1_img from '../assets/Tomato.png'
 import project2_img from '../assets/Blogger.png'
 import project3_img from '../assets/Hospital_Management.png'
-import project4_img from '../assets/Murder_Mystrey.png'
+import project4_img from '../assets/Hunger-Helpers.png'
 import project5_img from '../assets/library.png'
 import project6_img from '../assets/Chat.png'
 
