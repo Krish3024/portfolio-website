dc6525d8457d9d7428ad6f1e56d5b4dc72d126ba
Krish Sahu <131345914+Krish3024@users.noreply.github.com>
2025-07-09 21:36:44 +0530

    Update mywork_data.js

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -2,7 +2,7 @@ import project1_img from '../assets/Tomato.png'
 import project2_img from '../assets/Blogger.png'
 import project3_img from '../assets/LPDR.png'
 import project4_img from '../assets/Hunger-Helpers.png'
-import project5_img from '../assets/Hunger-Helpers.png'
+import project5_img from '../assets/library.png'
 import project6_img from '../assets/Chat.png'
 
 const mywork_data = [
@@ -38,7 +38,7 @@ const mywork_data = [
     },
     {
         w_no:6,
-        w_name:"Frontend Tindog Project",
+        w_name:"Libathon Project",
         w_img:project5_img,
         w_link:"https://libathon.vercel.app/"
     },
